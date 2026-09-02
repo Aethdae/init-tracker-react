a9d0bbf0890e820e444b797118a02fd187de9731
Tyler Long <aethdae@gmail.com>
2026-09-01 21:00:33 -0500

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,71 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import InitCard from "./components/InitCard";
+let id = 0;
 
 export default function App() {
-  const [initCards, setInitCards] = useState([]);
+  const [initCards, setInitCards] = useState(() => {
+    const saveData = localStorage.getItem("save");
+    if (saveData) {
+      const arr = JSON.parse(saveData).map((item) => createCardFromData(item));
+      return arr;
+    }
+    return [];
+  });
+  const cardsRef = useRef(initCards);
+  cardsRef.current = initCards;
+
+  useEffect(() => {
+    setTimeout(() => {
+      autosave();
+    }, 10000);
+  }, []);
+
+  function autosave() {
+    localStorage.setItem("save", JSON.stringify(cardsRef.current));
+    setTimeout(() => {
+      autosave();
+    }, 10000);
+  }
+
+  function getNextId() {
+    id++;
+    return id;
+  }
+  function createBlankCard() {
+    return {
+      id: getNextId(),
+      initiative: 0,
+      name: "",
+      hp: 0,
+      notes: "",
+      effects: "",
+    };
+  }
+
+  function updateCard(newCard) {
+    const changed = initCards.map((card) =>
+      card.id === newCard.id ? newCard : card,
+    );
+    setInitCards((prev) => changed);
+  }
+
+  function createCardFromData(data) {
+    return {
+      id: getNextId(),
+      initiative: data.initiative,
+      name: data.name,
+      hp: data.hp,
+      notes: data.notes,
+      effects: data.effects,
+    };
+  }
+
+  function removeCard(id) {
+    const filter = initCards.filter((card) => card.id === id);
+    setInitCards((prev) => [...prev.splice(prev.indexOf(filter), 1)]);
+  }
   function addInit() {
-    setInitCards((prev) => [...prev]);
+    setInitCards((prev) => [...prev, createBlankCard()]);
   }
   return (
     <main className="max-w-300 mx-auto flex flex-col gap-4">
@@ -20,15 +81,24 @@ export default function App() {
         <h2>-</h2>
       </div>
       <hr />
-      <section className="flex flex-col mx-auto gap-4" id="initiativeHolder">
-        {initCards.map((card) => (
-          <InitCard key={card.id} item={card} />
-        ))}
+      <section className="flex flex-col mx-2 gap-4" id="initiativeHolder">
+        {initCards
+          .sort((a, b) => Number(b.initiative) - Number(a.initiative))
+          .map((card) => {
+            return (
+              <InitCard
+                key={card.id}
+                item={card}
+                removeItem={removeCard}
+                updateCard={updateCard}
+              />
+            );
+          })}
       </section>
       <button
         className="border-2 border-black rounded-2xl px-8 py-2 bg-green-400 font-bold self-center"
         id="addItemButton"
-        onClick={() => addInit}
+        onClick={addInit}
       >
         Add
       </button>
--- src/components/InitCard.jsx
+++ src/components/InitCard.jsx
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function InitCard({ item }) {
-  return <div>InitCard</div>;
+export default function InitCard({ item, removeItem, updateCard }) {
+  const [card, setCard] = useState(item);
+  // const [init, setInit] = useState(0);
+  // const [name, setName] = useState("");
+  // const [hp, setHp] = useState(0);
+  // const [notes, setNotes] = useState("");
+  // const [effects, setEffects] = useState("");
+
+  function updateValue(event) {
+    const { name, value } = event.target;
+    setCard({ ...card, [name]: value });
+    updateCard({ ...card, [name]: value });
+  }
+
+  return (
+    <div className="flex mx-4 gap-4">
+      <input
+        className="border border-black rounded-md px-2 py-1"
+        type="text"
+        name="initiative"
+        id="initiative"
+        value={card.initiative}
+        onChange={(e) => {
+          updateValue(e);
+        }}
+      />
+      <input
+        className="border border-black rounded-md px-2 py-1"
+        type="text"
+        name="name"
+        id="name"
+        value={card.name}
+        onChange={(e) => {
+          updateValue(e);
+        }}
+      />
+
+      <input
+        className="border border-black rounded-md px-2 py-1"
+        type="text"
+        name="hp"
+        id="hp"
+        value={card.hp}
+        onChange={(e) => {
+          updateValue(e);
+        }}
+      />
+      <input
+        className="border border-black rounded-md px-2 py-1"
+        type="text"
+        name="notes"
+        id="notes"
+        value={card.notes}
+        onChange={(e) => {
+          updateValue(e);
+        }}
+      />
+      <input
+        className="border border-black rounded-md px-2 py-1"
+        type="text"
+        name="effects"
+        id="effects"
+        value={card.effects}
+        onChange={(e) => {
+          updateValue(e);
+        }}
+      />
+      <button
+        onClick={() => removeItem(item.id)}
+        className="px-4 py-2 bg-red-400 font-bold border-2 border-black rounded-2xl"
+      >
+        Delete
+      </button>
+    </div>
+  );
 }
